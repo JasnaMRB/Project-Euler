90dce2f1b6e8515883ec613e99a7c8208170c596
jasnamrb <j.rodulfa@gmail.com>
2016-02-21 00:51:19 -0500

    Refactor Problem3

--- src/Problem3.java
+++ src/Problem3.java
@@ -8,27 +8,18 @@ public class Problem3 {
     public static void main(String args[]) {
         double num = 600851475143.0;
         List<Double> factors = new ArrayList<>();
-        for (double i = 3; i < Math.sqrt(num); i++) {
-            if (num % i == 0) {
-                factors.add(i);
-            }
-        }
+        for (double i = 3; i < Math.sqrt(num); i++)
+            if (num % i == 0) factors.add(i);
         List<Double> primeFactors = new ArrayList<>();
         factors.forEach(factor -> {
             boolean prime = true;
-            for (double i = 3; i <= Math.sqrt(factor);i++) {
-                if (factor % i == 0) {
-                   prime = false;
-                }
-            }
-            if (prime)
-                if (!primeFactors.contains(factor))
-                    primeFactors.add(factor);
+            for (double i = 3; i <= Math.sqrt(factor);i++)
+                if (factor % i == 0) prime = false;
+            if (prime) primeFactors.add(factor);
         });
         double largest = primeFactors.get(0);
-        for (int i = 0; i < primeFactors.size(); i++) {
+        for (int i = 0; i < primeFactors.size(); i++)
             if (primeFactors.get(i) > largest) largest = primeFactors.get(i);
-        }
 
         System.out.println(largest);
     }
